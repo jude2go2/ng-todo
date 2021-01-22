ba095ec845473070b53dd929fad8af8459951eb6
didimango <53489397+didimango@users.noreply.github.com>
2021-01-22 14:57:15 +0200

    added uuid package, form sends the data to service

--- src/app/components/new-todo/new-todo.component.ts
+++ src/app/components/new-todo/new-todo.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Form } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ITodo } from 'src/app/models/todo.interface';
+import { TodoService } from 'src/app/services/todo.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-new-todo',
@@ -7,13 +11,29 @@ import { Form } from '@angular/forms';
   styleUrls: ['./new-todo.component.scss'],
 })
 export class NewTodoComponent implements OnInit {
-  @ViewChild('f') form: Form;
+  @ViewChild('f') form: NgForm;
 
-  constructor() {}
+  constructor(private todoService: TodoService, public dialog: MatDialog) {}
 
   ngOnInit(): void {}
 
   public onNewTodoSubmit(): void {
+    //create the new todo obj
+    //send object to service
+    const formValues = this.form.form.value;
+
+    const newTodo: ITodo = {
+      id: uuidv4(),
+      title: formValues.title,
+      description: formValues.description,
+      endDate: formValues.date,
+      isCompleted: false,
+      isArchived: false,
+      selected: false,
+    };
+
+    this.todoService.addNewTodo(newTodo);
+    this.dialog.closeAll();
     console.log('On Submit ');
     console.log(this.form);
   }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -6,87 +6,14 @@ import { ITodo } from '../models/todo.interface';
   providedIn: 'root',
 })
 export class TodoService {
-  private mock: ITodo[] = [
-    {
-      id: 1,
-      title: 'Long-billed corella',
-      description: 'Cacatua tenuirostris',
-      isCompleted: false,
-      isArchived: true,
-      endDate: '4/18/2020',
-      selected: true,
-    },
-    {
-      id: 2,
-      title: 'Ibis, sacred',
-      description: 'Threskionis aethiopicus',
-      isCompleted: false,
-      isArchived: true,
-      endDate: '6/14/2020',
-      selected: false,
-    },
-    {
-      id: 3,
-      title: 'Bird, magnificent frigate',
-      description: 'Fregata magnificans',
-      isCompleted: false,
-      isArchived: false,
-      endDate: '6/26/2020',
-      selected: false,
-    },
-    {
-      id: 4,
-      title: 'Common brushtail possum',
-      description: 'Trichosurus vulpecula',
-      isCompleted: false,
-      isArchived: false,
-      endDate: '6/5/2020',
-      selected: false,
-    },
-    {
-      id: 5,
-      title: 'Banded mongoose',
-      description: 'Mungos mungo',
-      isCompleted: false,
-      isArchived: false,
-      endDate: '12/4/2020',
-      selected: false,
-    },
-    {
-      id: 6,
-      title: 'Jacana, african',
-      description: 'Actophilornis africanus',
-      isCompleted: false,
-      isArchived: true,
-      endDate: '10/11/2020',
-      selected: false,
-    },
-    {
-      id: 7,
-      title: 'Eagle, african fish',
-      description: 'Haliaetus vocifer',
-      isCompleted: false,
-      isArchived: false,
-      endDate: '12/13/2020',
-      selected: false,
-    },
-    {
-      id: 8,
-      title: 'Tortoise, asian foreset',
-      description: 'Manouria emys',
-      isCompleted: false,
-      isArchived: false,
-      endDate: '4/30/2020',
-      selected: false,
-    },
-  ];
+  private todos: Array<ITodo> = [];
 
   private _todoSubject: BehaviorSubject<Array<ITodo>> = new BehaviorSubject(
-    this.mock
+    this.todos
   );
 
   private _singleTodoSubject: BehaviorSubject<ITodo> = new BehaviorSubject(
-    this.mock[0]
+    this.todos.length ? this.todos[0] : null
   );
 
   constructor() {}
@@ -101,5 +28,15 @@ export class TodoService {
 
   public setSelectedTodo(todo: ITodo) {
     this._singleTodoSubject.next(todo);
+  }
+
+  public addNewTodo(newTodo: ITodo): void {
+    //take exiting todos
+    //add new todo to exiting todos
+    //trigger next tic in observable;
+    console.log(newTodo);
+    const exitistingTodos: Array<ITodo> = this._todoSubject.value;
+    exitistingTodos.push(newTodo);
+    this._todoSubject.next(exitistingTodos);
   }
 }
